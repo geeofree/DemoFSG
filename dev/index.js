55b44bd11006e3780462cb7dff901fc18b910cce
Geoffrey <geo.galagaran@gmail.com>
2017-09-12 07:33:24 +0800

    Add dev directory

---
+++ dev/index.js
@@ -0,0 +1,7 @@
+import React    from 'react'
+import ReactDOM from 'react-dom'
+
+const Hello = () => <h1>Hello World!</h1>
+const root = document.getElementById('root')
+
+ReactDOM.render(<Hello />, root)
