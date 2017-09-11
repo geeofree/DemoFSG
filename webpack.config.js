560e4d575de1a042ad7c437ef3abea6ff5f229ed
Geoffrey <geo.galagaran@gmail.com>
2017-09-12 07:30:55 +0800

    Create config

---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+const Webpack = require('webpack')
+const path = require('path')
+
+const ExtractCSS = new ExtractTextPlugin('style.css')
+const WebpackHMR = new Webpack.HotModuleReplacementPlugin()
+
+const DevPath  = path.join(__dirname, 'dev')
+const ProdPath = path.join(__dirname, 'prod')
+
+module.exports = {
+	entry: DevPath,
+	output: {
+		path: ProdPath,
+		filename: 'bundle.js'
+	},
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				use: 'babel-loader',
+				exclude: /node_modules/
+			},
+			{
+				test: /\.s(a|c)ss/,
+				use: ExtractCSS.extract(['css-loader', 'postcss-loader', 'sass-loader'])
+			}
+		]
+	},
+	devServer: {
+		hot: true,
+		port: 3000,
+		compress: true,
+		contentBase: DevPath,
+		historyApiFallback: {
+			index: 'index.html'
+		}
+	},
+	plugins: [
+		ExtractCSS,
+		WebpackHMR
+	]
+}
