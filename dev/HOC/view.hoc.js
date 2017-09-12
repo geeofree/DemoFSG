8c48f2892df01061818f7fd5baf32e482af61f61
Geoffrey <geo.galagaran@gmail.com>
2017-09-12 11:49:54 +0800

    Change route component rendering to use the component prop instead of the render prop

--- dev/HOC/view.hoc.js
+++ dev/HOC/view.hoc.js
@@ -2,5 +2,5 @@ import React from 'react'
 import { Route } from 'react-router-dom'
 
 export default (WrappedComponent) => (props) => (
-	<Route {...props} render={WrappedComponent} />
+	<Route {...props} component={WrappedComponent} />
 )
