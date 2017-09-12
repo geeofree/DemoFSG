3a911a9a62804d6d270f7a30c34fe064e9d25483
Geoffrey <geo.galagaran@gmail.com>
2017-09-12 12:38:02 +0800

    Create Inventory view

--- dev/views/Inventory/index.js
+++ dev/views/Inventory/index.js
@@ -1,8 +1,39 @@
 import React from 'react'
-import ViewHOC from '../../HOC/view.hoc'
+import { connect } from 'react-redux'
+import './inventory.style.sass'
 
-const InventoryView = () => (
-	<h1>You are in the Inventory View</h1>
+import ViewHOC from '../../HOC/view.hoc'
+import Navbar  from '../../components/Routings/Navbar'
+
+const InventoryView = ({ items }) => (
+	<div id="inventory">
+		<Navbar />
+
+		{items.map((item, i) => (
+			<div className="item" key={i}>
+				<div className="item-img-container" />
+
+				<div className="item-detail-container">
+					<h1 className="item-name">{item.name}</h1>
+
+					<div className="item-price-stock">
+						<p className="item-price">&#8369;{item.price}</p>
+
+						<p className="item-stock">
+							<span style={{ fontWeight: 'bold' }}>In stock: </span>
+							{item.stock}
+						</p>
+					</div>
+				</div>
+			</div>
+		))}
+	</div>
 )
 
-export default ViewHOC(InventoryView)
+const mapStateToProps = ({ inventory }) => ({
+	items: inventory.items
+})
+
+const Inventory = connect(mapStateToProps)(InventoryView)
+
+export default ViewHOC(Inventory)
