af5011a97a25566ccb0e3856d2cfcbe978d22ee1
Geoffrey <geo.galagaran@gmail.com>
2017-09-12 11:49:15 +0800

    Change inventory initialState

--- dev/reducers/inventory.reducer.js
+++ dev/reducers/inventory.reducer.js
@@ -1,4 +1,26 @@
-const initialState = {}
+const initialState = {
+	selectedItem: null,
+	items: [
+		{
+			id: 0,
+			name: "Foo",
+			price: 125.00,
+			stock: 5
+		},
+		{
+			id: 1,
+			name: "Bar",
+			price: 12.00,
+			stock: 2
+		},
+		{
+			id: 2,
+			name: "Baz",
+			price: 25.20,
+			stock: 0
+		}
+	]
+}
 
 export default (state=initialState, action) => {
 	switch(action.type) {
