166ba06d020ffb50bf31d6b5d72dd1d3eee9a607
Geoffrey <geo.galagaran@gmail.com>
2017-09-12 11:08:37 +0800

    Change ItemView component

--- dev/views/Item/index.js
+++ dev/views/Item/index.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import ViewHOC from '../../HOC/view.hoc'
 
-const ItemView = (props) => (
-	<h1>You are in the Item View {console.log(props)}</h1>
+const ItemView = () => (
+	<h1>You are in the Item View</h1>
 )
 
 export default ViewHOC(ItemView)
